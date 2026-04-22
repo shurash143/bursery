fb37eeff49c28e271ea29d50f732160f13b96ea6
shurash143 <shukrigarad32@gmail.com>
2026-04-22 11:58:02 +0300

    connect frontend to backend API

--- frontend/src/api/axiosConfig.js
+++ frontend/src/api/axiosConfig.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://127.0.0.1:5000/api",
+  baseURL: "https://bursery.onrender.com/api",
   headers: {
     "Content-Type": "application/json",
   },
@@ -9,7 +9,7 @@ const API = axios.create({
 
 // Attach token automatically
 API.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token"); // Make sure you store token after login
+  const token = localStorage.getItem("token");
   if (token) {
     config.headers.Authorization = `Bearer ${token}`;
   }
